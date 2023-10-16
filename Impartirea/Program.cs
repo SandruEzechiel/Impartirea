99eb4c05cf64db40d14d62097b844d2ed43d9bdc
SabdruEzechiel <ezechiel.sandru@emanuel.ro>
2023-10-16 22:34:14 +0300

    a

---
+++ Impartirea/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Impartirea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+           
+            x = int.Parse(Console.ReadLine());
+           
+            y = int.Parse(Console.ReadLine());
+            
+            if (x % y != 0)
+                
+                Console.WriteLine("Nu se poate efectua impartirea exacta");
+            else
+                Console.WriteLine(x / y);
+        }
+    }
+}
